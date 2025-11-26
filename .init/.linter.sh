3e4bed6d2d5420403140da1a521d23c5b0a68555
Kavia AI <production@kavia.ai>
2025-11-26 12:03:44 +0000

    Checkpoint 1 - cge67c92da

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/open-source-book-reader-283200-283210/kindle_clone_frontend
+flutter analyze
+
---
+++ kindle_clone_frontend/android/app/src/main/kotlin/com/example/kindle_clone_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kindle_clone_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
